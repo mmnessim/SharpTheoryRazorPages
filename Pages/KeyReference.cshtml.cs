745f80f493d9d9c67f185eaf49edf0f13a1069d6
Mounir Nessim <mmnessim@gmail.com>
2025-09-14 18:42:14 -0400

    Move keys to /KeyReference

--- Pages/KeyReference.cshtml.cs
+++ Pages/KeyReference.cshtml.cs
@@ -5,26 +5,16 @@ using System.Text.Json;
 
 namespace SharpTheory.Pages
 {
-    public class IndexModel : PageModel
+    public class KeyReferenceModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-        public DateTime CurrentTime { get; set; }
         public TheoryDescription? Description { get; set; }
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
+        public List<TheoryKey>? Keys { get; set; }
         public void OnGet()
         {
             var json = System.IO.File.ReadAllText("Data/data.json");
             var root = JsonSerializer.Deserialize<TheoryRoot>(json);
             Description = root?.Description;
-
-            CurrentTime = DateTime.Now;
-
-            _logger.LogInformation("Index page loaded at {Time}", CurrentTime);
+            Keys = root?.Keys;
         }
     }
 }
