0674db4b0244e1125635db4121040d0e03a0d871
Mounir Nessim <mmnessim@gmail.com>
2025-09-14 13:25:28 -0400

    Add name and date to License, fix logic in KeyDetails.cshtml.cs

--- Pages/Keys/KeyDetails.cshtml.cs
+++ Pages/Keys/KeyDetails.cshtml.cs
@@ -7,18 +7,20 @@ namespace SharpTheory.Pages
 {
     public class KeyDetailsModel : PageModel
     {
+
+
         public TheoryKey? Key { get; set; }
         public TheoryScaleType? Scale { get; set; }
         public void OnGet(string id)
         {
             var json = System.IO.File.ReadAllText("Data/data.json");
             var root = JsonSerializer.Deserialize<TheoryRoot>(json);
-            var parts = id.Split("-");
-            var tonic = parts[0];
-            var mode = parts[1];
-            Key = root?.Keys.FirstOrDefault(k => k.Name.ToLower().Contains(tonic) && k.Mode.Contains(mode));
+            var noteName = id.Split("-")[0];
+            var mode = id.Split("-")[1];
+            id = id.Replace("-", " ");
+            Key = root?.Keys.FirstOrDefault(k => k.Name.ToLower() == id && k.Mode.Contains(mode));
 
-            var scales = root?.Scales.FirstOrDefault(s => s.Integer == Key?.Integer);
+            var scales = root?.Scales.FirstOrDefault(s => s.Integer == Key?.Integer && s.Tonic.ToLower().Contains(noteName));
             if (mode == "major")
             {
                 Scale = scales?.Major;
@@ -26,6 +28,7 @@ namespace SharpTheory.Pages
             {
                 Scale = scales?.NaturalMinor;
             }
+
         }
     }
 }
