70cc749df599864ce58f2faf4f162e3c2d75f40d
Mounir Nessim <mmnessim@gmail.com>
2025-09-16 14:15:38 -0400

    Add a couple tests

--- SharpTheoryTest/UnitTest1.cs
+++ SharpTheoryTest/UnitTest1.cs
@@ -20,6 +20,28 @@ namespace SharpTheoryTest
             Assert.NotNull(model.Key);
             Assert.NotNull(model.Scale);
             Assert.Null(model.NatMinor);
+
+            exception = Record.Exception(() => model.OnGet("c-minor"));
+
+            Assert.Null(exception);
+            Assert.Equal("C minor", model.Key?.Name);
+            Assert.Equal("minor", model.Key?.Mode);
+            Assert.NotNull(model.Key);
+            Assert.NotNull(model.Scale);
+            Assert.NotNull(model.NatMinor);
+        }
+
+        [Fact]
+        public void OnGet_WithInvalidId()
+        {
+            var model = new KeyDetailsModel();
+
+            var exception = Record.Exception(() => model.OnGet("INVALID"));
+            Assert.NotNull(exception);
+            Console.WriteLine(exception.Message);
+            Assert.Null(model.Key);
+            Assert.Null(model.Scale);
+            Assert.Null(model.NatMinor);
         }
     }
 }
