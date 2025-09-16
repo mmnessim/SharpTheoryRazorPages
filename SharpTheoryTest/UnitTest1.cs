79c59dfedd906f3f1e24093cce39da1b918146ad
Mounir Nessim <mmnessim@gmail.com>
2025-09-16 05:55:41 -0400

    Move git root into parent folder

---
+++ SharpTheoryTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using SharpTheory.Models;
+using SharpTheory.Pages;
+using System.Text.Json;
+using Xunit;
+
+namespace SharpTheoryTest
+{
+    public class KeyDetailsModelsTests
+    {
+        [Fact]
+        public void OnGet_WithValidId_SetsKeyAndScale()
+        {
+            var model = new KeyDetailsModel();
+
+            var exception = Record.Exception(() => model.OnGet("c-major"));
+
+            Assert.Null(exception);
+            Assert.Equal("C Major", model.Key?.Name);
+            Assert.Equal("major", model.Key?.Mode);
+            Assert.NotNull(model.Key);
+            Assert.NotNull(model.Scale);
+            Assert.Null(model.NatMinor);
+        }
+    }
+}
