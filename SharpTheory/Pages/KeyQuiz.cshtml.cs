4118b1de8c69c8f5de6735a12de75306c0169de0
Mounir Nessim <mmnessim@gmail.com>
2025-09-16 14:50:55 -0400

    Add quiz options (only sharps/flats)

--- SharpTheory/Pages/KeyQuiz.cshtml.cs
+++ SharpTheory/Pages/KeyQuiz.cshtml.cs
@@ -7,6 +7,7 @@ namespace SharpTheory.Pages
 {
     public class KeyQuizModel : PageModel
     {
+        private List<TheoryKey>? KeyPool { get; set; }
         public TheoryKey? Key { get; set; }
         public int? NumSharps { get; set; }
         public int? NumFlats { get; set; }
@@ -20,16 +21,37 @@ namespace SharpTheory.Pages
         public int? UserFlats { get; set; }
         public string? ResultMessage { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public bool OnlySharps { get; set; } = false;
+        [BindProperty(SupportsGet = true)]
+        public bool OnlyFlats { get; set; } = false;
+
         public void OnGet()
         {
             var json = System.IO.File.ReadAllText("Data/data.json");
             var root = JsonSerializer.Deserialize<TheoryRoot>(json);
+            if (OnlySharps)
+            {
+                KeyPool = root?.Keys.Where(k => k.NumSharps > 0).ToList();
+            }
+            else if (OnlyFlats)
+            {
+                KeyPool = root?.Keys.Where(k => k.NumFlats > 0).ToList();
+            }
+            else
+            {
+                KeyPool = root?.Keys;
+            }
 
             var random = new Random();
-            Key = root?.Keys[random.Next(root.Keys.Count)];
-            NumSharps = Key?.NumSharps;
-            NumFlats = Key?.NumFlats;
-            SelectedKeyName = Key?.Name;
+            if (KeyPool != null)
+            {
+                Key = KeyPool[random.Next(KeyPool.Count)];
+                NumSharps = Key?.NumSharps;
+                NumFlats = Key?.NumFlats;
+                SelectedKeyName = Key?.Name;
+            }
+            
         }
 
         public void OnPost()
