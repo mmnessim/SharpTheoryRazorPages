4688c4db8a72d570006b8a02f033aaa52dd93c5e
mmnessim <119817779+mmnessim@users.noreply.github.com>
2025-09-24 18:29:27 +0000

    more doc comments'

--- SharpTheory/Pages/KeyReference.cshtml.cs
+++ SharpTheory/Pages/KeyReference.cshtml.cs
@@ -5,10 +5,24 @@ using System.Text.Json;
 
 namespace SharpTheory.Pages
 {
+    /// <summary>
+    /// PageModel for the Key Reference page, loads and displays key signatures and descriptions.
+    /// </summary>
     public class KeyReferenceModel : PageModel
     {
+        /// <summary>
+        /// Description of API
+        /// </summary>
         public TheoryDescription? Description { get; set; }
+
+        /// <summary>
+        /// List of all keys from data.json
+        /// </summary>
         public List<TheoryKey>? Keys { get; set; }
+
+        /// <summary>
+        /// Handles GET requests to load key data from JSON file.
+        /// </summary>
         public void OnGet()
         {
             var json = System.IO.File.ReadAllText("Data/data.json");
