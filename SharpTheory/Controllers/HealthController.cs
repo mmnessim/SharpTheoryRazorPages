f1eced034f283baa0cc31ead64089d0c39f319cc
Mounir Nessim <mmnessim@gmail.com>
2025-10-28 17:02:44 -0400

    Add HealthController and HealthModel for health check endpoint

---
+++ SharpTheory/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharpTheory.Models;
+
+namespace SharpTheory.Controllers;
+
+[ApiController]
+public class HealthController
+{
+
+    [HttpGet("/health")]
+    public HealthModel HealthCheck()
+    {
+        return new HealthModel("healthy", 200);
+    }
+}
---
+++ SharpTheory/Models/HealthModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpTheory.Models;
+
+public class HealthModel
+{
+    public string Message { get; set; }
+    public int StatusCode { get; set; }
+
+    public HealthModel(string message, int statusCode)
+    {
+        Message = message;
+        StatusCode = statusCode;
+    }
+}
